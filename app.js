b78eda5b7a48064ebcdff342cc4e805e42e2b367
Ayman Rayan Kissami <82874742+Ayman-Rayan@users.noreply.github.com>
2021-09-02 23:00:44 +0100

    added a counter

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log("Hello mdfk");
+let add=document.getElementById('increase');
+let remove=document.getElementById('decrease');
+let zero=document.getElementById('reset');
+
+let int =document.getElementById('number');
+let integer=0;
+
+add.addEventListener('click',function(){
+    integer+=1;
+    int.innerHTML=integer;
+});
+
+remove.addEventListener('click',function(){
+     integer-=1;
+     int.innerHTML=integer;
+});
+
+zero.addEventListener('click',function(){
+    integer=0;
+    int.innerHTML= integer;
+});
+
+
